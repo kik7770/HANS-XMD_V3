6c5338690aeaee23bf0748ada4e61e411576a8bf
kik7770 <liju3237@gmail.com>
2025-06-14 19:40:50 -0400

    Update config.cjs

--- config.cjs
+++ config.cjs
@@ -5,7 +5,7 @@ const fs = require("fs");
 require("dotenv").config();
 
 const config = {
-  SESSION_ID: process.env.SESSION_ID || "",
+  SESSION_ID: process.env.SESSION_ID || "HansTz&q8hjSbwJ#YzRjCw2DQhElLUyo75e81ktK-HA5YapwQ-nG4poNHw8",
   PREFIX: process.env.PREFIX || '.',
   AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN !== undefined ? process.env.AUTO_STATUS_SEEN === 'true' : true, 
   AUTO_DL: process.env.AUTO_DL !== undefined ? process.env.AUTO_DL === 'true' : false,
