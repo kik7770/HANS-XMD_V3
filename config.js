c11e555bed15b624fc619c26e7ce2bb00af2a593
kik7770 <liju3237@gmail.com>
2025-06-14 19:36:00 -0400

    Update config.js

--- config.js
+++ config.js
@@ -11,7 +11,7 @@ const DATABASE_URL = process.env.DATABASE_URL === undefined
 module.exports = { session: process.env.SESSION_ID || 'HansTz&q8hjSbwJ#YzRjCw2DQhElLUyo75e81ktK-HA5YapwQ-nG4poNHw8',
     PREFIXE: process.env.PREFIX || ".",
     OWNER_NAME: process.env.OWNER_NAME || "HANS-XMD",
-    NUMERO_OWNER : process.env.NUMERO_OWNER || " 263717087382",              
+    NUMERO_OWNER : process.env.NUMERO_OWNER || " 50944562059",              
     AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "yes",
     AUTO_DOWNLOAD_STATUS: process.env.AUTO_DOWNLOAD_STATUS || 'no',
     BOT : process.env.BOT_NAME || 'HANS-XMD',
